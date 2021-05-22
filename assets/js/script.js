46c1806a55c585c01b4f3d99f40214ee27290a8b
rami khoualdi <ramykhweldi@gmail.com>
2021-05-22 03:33:27 +0100

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-
+// open/ close Left Navigation in desktop version
+var menu_box = document.getElementById('menu_box')
+var mobile_nav_bar = document.getElementsByClassName('mobile_nav')[0]
+var menu = "close";
+menu_box.addEventListener('click', function(){
+    if(menu === 'close'){
+        mobile_nav_bar.style.transform = 'translate(0, 0)'
+        menu = 'open' 
+    }else{
+        mobile_nav_bar.style.transform = 'translate(-100%, 0)'
+        menu = 'close'
+    }
+})
+var close_drawer = document.getElementById('close_drawer')
+close_drawer.addEventListener('click', function(){
+    mobile_nav_bar.style.transform = 'translate(-100%, 0)'
+    menu = 'close'
+})
 
